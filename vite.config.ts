c8ccddc3c983b762951d3e64a90257f7e4e071ab
Dov Rosenberg <dov@rosenbergmail.com>
2025-07-01 18:44:05 -0400

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(({ mode }) => {
   const isDevelopment = mode === 'development'; 
 
   return {
-    base: './',
+    // base: './',
     resolve: {
       alias: [
         {
